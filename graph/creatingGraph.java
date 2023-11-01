f9a309646828818b1a2bab67b635053ed0ec1637
Vaibhav Baneshi <vaibhavbaneshi@gmail.com>
2023-11-01 23:02:18 +0530

    creatingGraph

--- graph/creatingGraph.java
+++ graph/creatingGraph.java
@@ -2,48 +2,31 @@ package graph;
 
 import java.util.ArrayList;
 
-public class creatingGraph {
-    
-    static class Edge {
-        int src;
-        int dest;
-        int wt;
-
-        public Edge(int src,int dest,int wt) {
-            this.src = src;
-            this.dest = dest;
-            this.wt = wt;
-        }
-    }
-
-    public static void insertInGraph(ArrayList<Edge> graph[]) {
-        for(int i=0;i<graph.length;i++) {
-            graph[i] = new ArrayList<>();
-        }
-
-        graph[0].add(new Edge(0,2,2));
-
-        graph[1].add(new Edge(1,2,10));
-        graph[1].add(new Edge(1, 3,0));
-
-        graph[2].add(new Edge(2, 0,2));
-        graph[2].add(new Edge(2, 1,10));
-        graph[2].add(new Edge(2, 3,-1));
-
-        graph[3].add(new Edge(3, 1,0));
-        graph[3].add(new Edge(3, 2,-1));
-    }
+class creatingGraph {
 
     public static void main(String[] args) {
-        int V = 4;
-        ArrayList<Edge> graph[] = new ArrayList[V];
 
-        insertInGraph(graph);
+        int n=3;
+        int m=3;
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
 
-        System.out.println("The neighbours of 2 are : ");
-        for(int i=0;i<graph[2].size();i++) {
-            Edge e = graph[2].get(i);
-            System.out.print(e.dest+" "+e.wt);
+        for(int i=0;i<=n;i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        adj.get(1).add(2);
+        adj.get(2).add(1);
+
+        adj.get(2).add(3);
+        adj.get(3).add(2);
+
+        adj.get(1).add(3);
+        adj.get(3).add(1);
+
+        for(int i=1;i<=n;i++) {
+            for(int j=0;j<adj.get(1).size();j++) {
+                System.out.print(adj.get(i).get(j)+" ");
+            }
             System.out.println();
         }
     }
