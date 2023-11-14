3dc0362f35ebbe85e4d57d5ef1e4aacbb481db14
Vaibhav Baneshi <vaibhavbaneshi@gmail.com>
2023-11-14 23:51:00 +0530

    graph

--- graph/isCycleUndirected.java
+++ graph/isCycleUndirected.java
@@ -1,38 +1,63 @@
 package graph;
 
-import java.util.ArrayList;  
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
-class isCycleUndefined {
+class Pair {
+    
+    int data;
+    int parent;
 
-    public static boolean dfs(ArrayList<ArrayList<Integer>> adj,boolean[] vis,int parent,int src) {
+    Pair(int data, int parent) {
+        this.data = data;
+        this.parent = parent;
+    }
+}
+
+class isCycleUndirected {
+
+    public static boolean checkCycle(ArrayList<ArrayList<Integer>> adj, boolean[] vis, int src) {
+
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(src, -1));
         vis[src] = true;
 
-        for(int it : adj.get(src)) {
-            if(!vis[it]) {
-                if(dfs(adj, vis, src, it))
-                    return true;
-            }
+        while(!q.isEmpty()) {
+            int data = q.peek().data;
+            int parent = q.peek().parent;
+            q.poll();
 
-            else if(parent != it)
+            for(int it : adj.get(data)) {
+                if(!vis[it]) {
+                    vis[it] = true;
+                    q.offer(new Pair(it,data));
+                }
+
+                else if(parent != it) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static boolean isCycle(ArrayList<ArrayList<Integer>> adj, int V) {
+
+        for(int i=0;i<V;i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        for(int i=0;i<V;i++) {
+            if(checkCycle(adj,new boolean[V],i))
                 return true;
         }
 
         return false;
     }
 
-    public static boolean isCycle(ArrayList<ArrayList<Integer>> adj,boolean[] vis,int parent,int V) {
-
-        for(int i=0;i<V;i++) {
-            if(!vis[i]) {
-                if(dfs(adj,vis,-1,i)) 
-                    return true;
-            } 
-        }
-
-        return false;
-    }
-
     public static void main(String[] args) {
-
+        
     }
 }
