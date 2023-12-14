142248f83016e7787bd24180e5b17ca101bb41d8
Vaibhav Baneshi <vaibhavbaneshi@gmail.com>
2023-12-14 10:15:38 +0530

    objects_javaScript

--- objects/02_objects.js
+++ objects/02_objects.js
@@ -21,3 +21,13 @@ const obj = {
 console.log(Object.keys(obj))
 console.log(Object.values(obj))
 console.log(Object.entries(obj))
+
+const course = {
+    name: "Learn JavaScript",
+    courseIntructor: "Vaibhav",
+    price: "999"
+}
+//Object Destructor
+const {courseIntructor: instructor} = course
+
+console.log(instructor);
